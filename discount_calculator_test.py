fe00e3de92d3c3cb7f6c56391d9181f81ae5118a
Mark Folashade <ayodeji.folashade@gmail.com>
2013-09-06 07:59:20 -0500

    Milestone 3

--- discount_calculator_test.py
+++ discount_calculator_test.py
@@ -12,3 +12,9 @@ class DiscountCalculatorTests(unittest.TestCase):
     result = discount_calculator.calculate(100,15,'percent')
     self.assertEqual(15.0, result)
 
+  def five_dollar_discount_test(self):
+    discount_calculator = DiscountCalculator()
+    result = discount_calculator.calculate(250,5,'absolute')
+    self.assertEqual(5, result)
+
+
